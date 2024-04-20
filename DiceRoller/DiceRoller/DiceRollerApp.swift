5f85d3e8ff107c652cd5710be4e512d9b8173721
Julien Juret <julien.juret@gmail.com>
2024-04-21 00:14:38 +0200

    Exercise DiceRoller
    Signed-off-by: Julien Juret <julien.juret@gmail.com>

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Julien Juret on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
